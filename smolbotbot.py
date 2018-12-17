2a11c8817178e9b1aeea4cb505652ff37ddceb9d
Tom Panton <tom@tomandtally.co.uk>
2018-12-17 21:54:11 +0000

    Checking for new robots, storing responded tweets and dms

--- smolbotbot.py
+++ smolbotbot.py
@@ -1,8 +1,33 @@
 import robots
+import robotdata
 import search
 import twitter
 import schedule
 import time
+
+
+responded_tweets = []
+responded_dms = []
+
+saved_responded_tweets = open("state/responded-tweets.txt", "r")
+for tweet_id in saved_responded_tweets:
+    try:
+        responded_tweets.append(int(tweet_id.strip()))
+    except ValueError:
+        continue
+saved_responded_tweets.close()
+del saved_responded_tweets
+print("Loaded responded tweets: " + str(responded_tweets))
+
+saved_responded_dms = open("state/responded-dms", "r")
+for dm_id in saved_responded_dms:
+    try:
+        responded_dms.append(int(dm_id.strip()))
+    except ValueError:
+        continue
+saved_responded_dms.close()
+del saved_responded_dms
+print("Loaded responded dms: " + str(responded_dms))
 
 
 def morning_tweet():
@@ -18,7 +43,11 @@ def tweet_next_robot(time_of_day):
 
 
 def check_new_robots():
-    print("Check for new robots")
+    recent_tweets = twitter.recent_tweets("smolrobots", 10800)
+    print(str(len(recent_tweets)) + " recent tweets found from @smolrobots, looking for new robots")
+    for tweet in recent_tweets:
+        if robotdata.generate_robot_data(tweet.text, tweet.id):
+            print("Registered a new robot")
 
 
 def check_tweets():
@@ -29,18 +58,41 @@ def check_direct_messages():
     print("Check DMs")
 
 
+def close_bot():
+    global responded_tweets, responded_dms
+
+    tweets_file = open("state/responded-tweets.txt", "w")
+    for tweet_id in responded_tweets:
+        tweets_file.write(str(tweet_id))
+    tweets_file.close()
+    print("Saved responded tweet ids")
+
+    dms_file = open("state/responded-dms", "w")
+    for dm_id in responded_dms:
+        dms_file.write(str(dm_id))
+    dms_file.close()
+    print("Saved responded dm ids")
+
+
 schedule.every().day.at("07:00").do(morning_tweet)
 schedule.every().day.at("12:00").do(noon_tweet)
 schedule.every().hour.do(check_new_robots)
 schedule.every().minute.do(check_direct_messages())
 schedule.every(15).seconds.do(check_tweets())
 
+
 while True:
     try:
+        time.sleep(1)
         schedule.run_pending()
+    except KeyboardInterrupt:
+        print("Keyboard interrupt, stopping")
+        close_bot()
+        break
     except:
-        print("An error occurred in schedule loop")
-    time.sleep(1)
+        print("An uncaught error occurred in schedule loop")
 
     # Todo: accept DM commands from @pantonshire
     # Todo: make sure to check for retweets and whether or not the tweet actually comes from @smolrobots
+
+print("Goodbye!")
--- twitter.py
+++ twitter.py
@@ -38,12 +38,15 @@ def reply(reply_to_tweet, message):
 
 def mentions(count, max_seconds_ago, id_blacklist):
     global api
-    return [mention for mention in api.mentions_timeline(count=count) if not mention.id in id_blacklist and (dt.datetime.now() - mention.created_at).seconds < max_seconds_ago]
+    return [mention for mention in api.mentions_timeline(count=count)
+            if not mention.id in id_blacklist and (dt.datetime.now() - mention.created_at).seconds < max_seconds_ago]
 
 
 def recent_tweets(user, max_seconds_ago):
     global api
-    return [(tweet.retweeted_status if hasattr(tweet, "retweeted_status") else tweet) for tweet in api.user_timeline("@" + user) if (dt.datetime.now() - tweet.created_at).seconds < max_seconds_ago]
+    return [(tweet.retweeted_status if hasattr(tweet, "retweeted_status") else tweet)
+            for tweet in api.user_timeline("@" + user)
+            if tweet.user.screen_name == user and (dt.datetime.now() - tweet.created_at).seconds < max_seconds_ago]
 
 
 def direct_messages(max_seconds_ago, id_blacklist):
@@ -52,7 +55,10 @@ def direct_messages(max_seconds_ago, id_blacklist):
         output = sp.run("/usr/local/bin/twurl -X GET /1.1/direct_messages/events/list.json".split(" "), stdout=sp.PIPE).stdout
         obj = json.loads(output.decode("utf-8"))
         messages = obj["events"]
-        return [message for message in messages if message["id"] not in id_blacklist and message["message_create"]["sender_id"] != bot_id and dt.datetime.now().timestamp() - (0.001 * float(message["created_timestamp"])) < max_seconds_ago]
+        return [message for message in messages
+                if message["id"] not in id_blacklist
+                and message["message_create"]["sender_id"] != bot_id
+                and dt.datetime.now().timestamp() - (0.001 * float(message["created_timestamp"])) < max_seconds_ago]
     except:
         print("Error retrieving direct messages")
         return []
