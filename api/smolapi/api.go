710519958c8d92be1e91e7538a72c7e77abb4339
Pantonshire <tom@tomandtally.co.uk>
2019-12-05 18:25:01 +0000

    Added a router and more HTTP responses

--- api/smolapi/api.go
+++ api/smolapi/api.go
@@ -41,35 +41,93 @@ func (api API) Close() {
 	defer closeRobotQueries(api.RobotQueries)
 }
 
+// NewRouter creates a new chi router for the API that can be mounted into the main router.
+func (api API) NewRouter() http.Handler {
+	router := chi.NewRouter()
+
+	router.Get("/latest/{n}", func(writer http.ResponseWriter, request *http.Request) {
+		if n, err := integerURLParam(writer, request, "n"); err == nil {
+			api.latest(writer, request, n)
+		}
+	})
+
+	router.Get("/latest/", func(writer http.ResponseWriter, request *http.Request) {
+		api.latest(writer, request, 1)
+	})
+
+	router.Get("/name/{name}", func(writer http.ResponseWriter, request *http.Request) {
+		api.simpleQuery(writer, request, "byname", "name")
+	})
+
+	router.Get("/prefix/{prefix}", func(writer http.ResponseWriter, request *http.Request) {
+		api.simpleQuery(writer, request, "byprefix", "prefix")
+	})
+
+	router.Get("/tag/{tag}", func(writer http.ResponseWriter, request *http.Request) {
+		tag := chi.URLParam(request, "tag")
+		centre, left, right := "% "+tag+" %", tag+" %", "% "+tag
+		result := runSelectRobots(api.Database, api.RobotQueries["bytag"], tag, centre, left, right)
+		robotsResponse(writer, request, result)
+	})
+
+	for _, valueName := range []string{"id", "number", "timestamp"} {
+		router.Get("/"+valueName+"/from/{from}/to/{to}", func(writer http.ResponseWriter, request *http.Request) {
+			api.numericValueRange(writer, request, valueName)
+		})
+
+		router.Get("/"+valueName+"/from/{from}", func(writer http.ResponseWriter, request *http.Request) {
+			api.numericValue(writer, request, "from", valueName)
+		})
+
+		router.Get("/"+valueName+"/to/{to}", func(writer http.ResponseWriter, request *http.Request) {
+			api.numericValue(writer, request, "to", valueName)
+		})
+
+		router.Get("/"+valueName+"/{by}", func(writer http.ResponseWriter, request *http.Request) {
+			api.numericValue(writer, request, "by", valueName)
+		})
+	}
+
+	return router
+}
+
 func robotsResponse(writer http.ResponseWriter, request *http.Request, robots []Robot) {
 	writer.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(writer).Encode(robots)
+}
+
+func integerURLParam(writer http.ResponseWriter, request *http.Request, paramName string) (int, error) {
+	n, err := strconv.Atoi(chi.URLParam(request, paramName))
+
+	if err != nil {
+		http.NotFound(writer, request)
+	}
+
+	return n, err
+}
+
+func (api API) simpleQuery(writer http.ResponseWriter, request *http.Request, query string, paramName string) {
+	param := chi.URLParam(request, paramName)
+	queryResult := runSelectRobots(api.Database, api.RobotQueries[query], param)
+	robotsResponse(writer, request, queryResult)
 }
 
 func (api API) latest(writer http.ResponseWriter, request *http.Request, numRobots int) {
 	robotsResponse(writer, request, runSelectRobots(api.Database, api.RobotQueries["latest"], numRobots))
 }
 
-// LatestRobot makes a json response of the latest small robot in the database.
-func (api API) LatestRobot(writer http.ResponseWriter, request *http.Request) {
-	api.latest(writer, request, 1)
-}
-
-// LatestRobots makes a json response of the latest n small robots in the database.
-func (api API) LatestRobots(writer http.ResponseWriter, request *http.Request) {
-	n, err := strconv.Atoi(chi.URLParam(request, "n"))
-
-	if err != nil {
-		http.NotFound(writer, request)
-	} else {
-		api.latest(writer, request, n)
+func (api API) numericValueRange(writer http.ResponseWriter, request *http.Request, valueName string) {
+	if from, err := integerURLParam(writer, request, "from"); err == nil {
+		if to, err := integerURLParam(writer, request, "to"); err == nil {
+			result := runSelectRobots(api.Database, api.RobotQueries["range"+valueName], from, to)
+			robotsResponse(writer, request, result)
+		}
 	}
 }
 
-// ByTag makes a json response of the small robots with the tag.
-func (api API) ByTag(writer http.ResponseWriter, request *http.Request) {
-	tag := chi.URLParam(request, "tag")
-	centre, left, right := "% "+tag+" %", tag+" %", "% "+tag
-	result := runSelectRobots(api.Database, api.RobotQueries["byTag"], tag, centre, left, right)
-	robotsResponse(writer, request, result)
+func (api API) numericValue(writer http.ResponseWriter, request *http.Request, searchType string, valueName string) {
+	if value, err := integerURLParam(writer, request, searchType); err == nil {
+		result := runSelectRobots(api.Database, api.RobotQueries[searchType+valueName], value)
+		robotsResponse(writer, request, result)
+	}
 }
--- api/smolapi/robotqueries.go
+++ api/smolapi/robotqueries.go
@@ -8,15 +8,15 @@ func makeRobotQueries(db *sql.DB) map[string]*sql.Stmt {
 	const table = "robots"
 
 	robotQueries := map[string]*sql.Stmt{
-		"byName":   prepareSelectAll(db, table, equalsCondition("name")),
-		"byPrefix": prepareSelectAll(db, table, equalsCondition("prefix")),
-		"byTag":    prepareSelectAll(db, table, "WHERE tags LIKE ? OR tags LIKE ? OR tags LIKE ? OR tags LIKE ?"),
+		"byname":   prepareSelectAll(db, table, equalsCondition("name")),
+		"byprefix": prepareSelectAll(db, table, equalsCondition("prefix")),
+		"bytag":    prepareSelectAll(db, table, "WHERE tags LIKE ? OR tags LIKE ? OR tags LIKE ? OR tags LIKE ?"),
 		"latest":   prepareSelectAll(db, table, "ORDER BY timestamp DESC LIMIT ?"),
 	}
 
-	addNumericQueries(db, table, robotQueries, "ID", "id")
-	addNumericQueries(db, table, robotQueries, "Number", "number")
-	addNumericQueries(db, table, robotQueries, "Timestamp", "timestamp")
+	addNumericQueries(db, table, robotQueries, "id")
+	addNumericQueries(db, table, robotQueries, "number")
+	addNumericQueries(db, table, robotQueries, "timestamp")
 
 	return robotQueries
 }
@@ -43,9 +43,9 @@ func rangeCondition(attribute string) string {
 	return "WHERE " + attribute + " >= ? AND " + attribute + " <= ?"
 }
 
-func addNumericQueries(db *sql.DB, table string, queries map[string]*sql.Stmt, keyName string, columnName string) {
-	queries["by"+keyName] = prepareSelectAll(db, table, equalsCondition(columnName))
-	queries["from"+keyName] = prepareSelectAll(db, table, fromCondition(columnName))
-	queries["to"+keyName] = prepareSelectAll(db, table, toCondition(columnName))
-	queries["range"+keyName] = prepareSelectAll(db, table, rangeCondition(columnName))
+func addNumericQueries(db *sql.DB, table string, queries map[string]*sql.Stmt, columnName string) {
+	queries["by"+columnName] = prepareSelectAll(db, table, equalsCondition(columnName))
+	queries["from"+columnName] = prepareSelectAll(db, table, fromCondition(columnName))
+	queries["to"+columnName] = prepareSelectAll(db, table, toCondition(columnName))
+	queries["range"+columnName] = prepareSelectAll(db, table, rangeCondition(columnName))
 }
