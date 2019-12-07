bdf03c5a015f8c1e0206523338cc8cbfbf567881
Pantonshire <tom@tomandtally.co.uk>
2019-12-07 12:13:39 +0000

    Update __init__.py for the new manual functions

--- smolbotbot/__init__.py
+++ smolbotbot/__init__.py
@@ -1,2 +1,2 @@
-from .manual import add_robot, search_for, time_task
+from .manual import add_robot, result_search, data_search, all_robot_data, time_task
 from .accounts import bot_handle, bot_id, support_handle, admin_ids
