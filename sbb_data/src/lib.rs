5b736c9fea4234aee78d94d84ae3e98d5095d2d2
Pantonshire <tom@tomandtally.co.uk>
2021-01-24 19:43:58 +0000

    Remove example test from sbb_data

--- sbb_data/src/lib.rs
+++ sbb_data/src/lib.rs
@@ -11,11 +11,3 @@ pub use crate::connect::connect_env;
 pub use crate::model::*;
 pub use crate::new::*;
 pub use crate::create::Create;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
