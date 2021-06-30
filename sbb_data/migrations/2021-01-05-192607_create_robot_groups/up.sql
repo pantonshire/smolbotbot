ec5ad3d467e95eae700ed9618dfffdd93008da4a
Pantonshire <tom@tomandtally.co.uk>
2021-06-30 13:19:05 +0100

    Image URL not null

--- sbb_data/migrations/2021-01-05-192607_create_robot_groups/up.sql
+++ sbb_data/migrations/2021-01-05-192607_create_robot_groups/up.sql
@@ -2,7 +2,7 @@ create table robot_groups (
     id               serial8 primary key,
     tweet_id         int8 not null unique,
     tweet_time       timestamp with time zone not null,
-    image_url        text,
+    image_url        text not null,
     body             text not null,
     alt              text,
     content_warning  text
